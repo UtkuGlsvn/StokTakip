8729326dfe95b64e9dfc800487a981bea96326e2
utkuglsvn <utku.glsvn@gmail.com>
2018-12-10 16:18:25 +0300

    frontend-1

--- src/main/java/indexBean.java
+++ src/main/java/indexBean.java
@@ -6,6 +6,24 @@ import javax.faces.bean.RequestScoped;
 public class indexBean {
 	private String username;
 	private int password;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getPassword() {
+		return password;
+	}
+
+	public void setPassword(int password) {
+		this.password = password;
+	}
+
 	public String login()
 	{
 	      return "login";
