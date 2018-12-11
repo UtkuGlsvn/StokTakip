316a5470956e9ddede272960660f858b95b465c5
utkuglsvn <utku.glsvn@gmail.com>
2018-12-11 18:04:14 +0300

    News xhtml Page

---
+++ src/main/java/urunBean.java
@@ -0,0 +1,18 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "urun", eager = true)
+@RequestScoped
+public class urunBean {
+
+	public String userAdd()
+	{
+		return null;
+          
+	}
+	public String userDelete()
+	{
+		return null;
+	}
+
+}
